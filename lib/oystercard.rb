c7894e1d634843d69133c3a56c326676d0fb342b
EvelinaClayton <clayton.evelina@gmail.com>
2019-07-30 14:51:51 +0100

    added maximum balance of 90 and identified CONSTANT

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,13 @@
 class Oystercard
+    MAXIMUM_BALANCE = 90
+
     attr_reader :balance
     def initialize 
         @balance = 0
     end 
 
     def top_up(amount)
+        fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded." if amount + balance > MAXIMUM_BALANCE
         @balance =+ amount
     end 
 end
