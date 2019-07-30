357359e0143fd8c46bdc2c874f8501e9931423fa
EvelinaClayton <clayton.evelina@gmail.com>
2019-07-30 23:24:32 +0100

    fixed rubocop errors

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 
 ruby '2.6.3'
 
 group :development, :test do
-  gem "rspec"
+  gem 'rspec'
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,49 +1,51 @@
-require "oystercard"
+# frozen_string_literal: true
+
+require 'oystercard'
 
 describe Oystercard do
   it 'has a balance of 0' do
     oystercard = Oystercard.new
     expect(oystercard.balance).to eq 0
-
   end
-  
+
   it 'has a method top_up' do
     oystercard = Oystercard.new
     expect(oystercard).to respond_to(:top_up).with(1).argument
   end
-  
+
   it 'can top up the balance' do
-    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    expect { subject.top_up 1 }.to change { subject.balance }.by 1
   end
-  
-  it 'raises an error if the maximum balance is exceeded' do 
-    maximum_balance = Oystercard::MAXIMUM_BALANCE 
+
+  it 'raises an error if the maximum balance is exceeded' do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
     subject.top_up(maximum_balance)
-    expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded."
+    error_message = "Maximum balance of #{maximum_balance} exceeded."
+    expect { subject.top_up 1 }.to raise_error(error_message)
   end
-  
+
   it 'has a method deduct' do
     expect(subject).to respond_to(:deduct).with(1).argument
   end
-  
-  it "has a method touch_in" do 
-    expect(subject).to respond_to(:touch_in)  
+
+  it 'has a method touch_in' do
+    expect(subject).to respond_to(:touch_in)
   end
-  
-  it "has a method touch_out" do 
-    expect(subject).to respond_to(:touch_out)  
+
+  it 'has a method touch_out' do
+    expect(subject).to respond_to(:touch_out)
   end
-  
-  it 'is initially not in a journey' do 
+
+  it 'is initially not in a journey' do
     expect(subject).not_to be_in_journey
   end
-  
-  it "can touch in" do
+
+  it 'can touch in' do
     subject.touch_in
     expect(subject).to be_in_journey
   end
 
-  it "can touch out" do
+  it 'can touch out' do
     subject.touch_in
     subject.touch_out
     expect(subject).not_to be_in_journey
