83b1b9f88fc84583b0e73084899a24f02e221ffa
monmon8 <student@Makerss-MacBook-Pro.local>
2019-07-31 15:07:25 +0100

    #10 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -17,20 +17,12 @@ class Oystercard
 
     @balance += amount
   end
-
-  def deduct(amount)
-    @balance -= amount
-  end
-
+  
   def touch_in
-    
-
     if @balance >= AMOUNT
     @in_journey = true
     else
-      raise "Not enough money"
-   
-      
+      raise "Not enough money" 
     end
     
   end
@@ -44,5 +36,9 @@ class Oystercard
 
   def in_journey?
     @in_journey
+  end
+   private
+  def deduct(amount)
+    @balance -= amount
   end
 end
